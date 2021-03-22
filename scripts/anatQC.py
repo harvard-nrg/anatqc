16c9f0156ca884b638db141235c16511eb2da3a7
Timothy O'Keefe <timothy_okeefe@harvard.edu>
2021-03-22 18:01:25 -0400

    add new auth arguments to anatQC.py

--- scripts/anatQC.py
+++ scripts/anatQC.py
@@ -12,14 +12,20 @@ def main():
     subparsers = parser.add_subparsers(help='sub-command help')
     # get mode
     parser_get = subparsers.add_parser('get', help='get -h')
-    parser_get.add_argument('--alias', default='cbscentral',
-        help='YAXIL authentication alias')
     parser_get.add_argument('--label', required=True,
         help='XNAT MR Session name')
     parser_get.add_argument('--project',
         help='XNAT Project name')
     parser_get.add_argument('--bids-dir', required=True,
         help='Output BIDS directory')
+    parser_get.add_argument('--xnat-alias',
+        help='YAXIL authentication alias')
+    parser_get.add_argument('--xnat-host',
+        help='XNAT host')
+    parser_get.add_argument('--xnat-user',
+        help='XNAT username')
+    parser_get.add_argument('--xnat-pass',
+        help='XNAT password')
     parser_get.set_defaults(func=cli.get.do)
     # process mode
     parser_process = subparsers.add_parser('process', help='process -h')
@@ -39,17 +45,25 @@ def main():
         help='BIDS run')
     parser_process.add_argument('--bids-dir', required=True,
         help='BIDS root directory')
-    parser_process.add_argument('--xnat-upload',
-        help='Upload XNAT archive to the provided XNAT')
     parser_process.add_argument('--dry-run', action='store_true',
         help='Do not actually execute any jobs')
     parser_process.add_argument('--sub-tasks', nargs='+', default=['morph', 'mriqc', 'vnav'],
         help='Run only certain sub tasks')
+    parser_process.add_argument('--xnat-alias',
+        help='YAXIL authentication alias')
+    parser_process.add_argument('--xnat-host',
+        help='XNAT host')
+    parser_process.add_argument('--xnat-user',
+        help='XNAT username')
+    parser_process.add_argument('--xnat-pass',
+        help='XNAT password')
+    parser_process.add_argument('--xnat-upload', action='store_true',
+        help='Create and upload XNAT XAR')
+    parser_process.add_argument('--keep-xar', action='store_true',
+        help='Do not delete XAR file')
     parser_process.set_defaults(func=cli.process.do)
     # tandem (run get and process)
     parser_tandem = subparsers.add_parser('tandem', help='tandem -h')
-    parser_tandem.add_argument('--alias', default='cbscentral',
-        help='YAXIL authentication alias')
     parser_tandem.add_argument('--label', required=True,
         help='XNAT MR Session name')
     parser_tandem.add_argument('--project',
@@ -64,12 +78,22 @@ def main():
         help='Choose a specific job scheduler')
     parser_tandem.add_argument('--rate-limit', type=int, default=None, 
         help='Rate limit the number of tasks executed in parallel (1=serial)')
-    parser_tandem.add_argument('--xnat-upload',
-        help='Upload XNAT archive to the provided XNAT')
     parser_tandem.add_argument('--dry-run', action='store_true',
         help='Do not actually execute any jobs')
     parser_tandem.add_argument('--sub-tasks', nargs='+', default=['morph', 'mriqc', 'vnav'],
         help='Run only certain sub tasks')
+    parser_tandem.add_argument('--xnat-alias',
+        help='YAXIL authentication alias')
+    parser_tandem.add_argument('--xnat-host',
+        help='XNAT host')
+    parser_tandem.add_argument('--xnat-user',
+        help='XNAT username')
+    parser_tandem.add_argument('--xnat-pass',
+        help='XNAT password')
+    parser_tandem.add_argument('--xnat-upload', action='store_true',
+        help='Create and upload XNAT XAR')
+    parser_tandem.add_argument('--keep-xar', action='store_true',
+        help='Do not delete XAR file')
     parser_tandem.set_defaults(func=cli.tandem.do)
     args = parser.parse_args()
 
